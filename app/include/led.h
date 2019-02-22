469c9e64b1efd2fa197f0d47d147973be8c10cd5
github150620 <13508348543@139.com>
2019-02-22 19:58:17 +0800

    Update led.h

--- app/include/led.h
+++ app/include/led.h
@@ -1,23 +1,28 @@
 #ifndef __LED_H__
 #define __LED_H__
 
+#define LED_BLINK_FREQ_0   0
+#define LED_BLINK_FREQ_0_5 0.5
+#define LED_BLINK_FREQ_1HZ 1.0
+#define LED_BLINK_FREQ_2HZ 2.0
+#define LED_BLINK_FREQ_4HZ 4.0
+#define LED_BLINK_FREQ_8HZ 8.0
+
+#define LED_BLINK_DUTY_FULL    1.0
+#define LED_BLINK_DUTY_HALF    0.5
+#define LED_BLINK_DUTY_QUARTER 0.25
+#define LED_BLINK_DUTY_EIGHTH  0.125
+#define LED_BLINK_DUTY_EMPTY   0.0
+
 typedef enum{
     LED_LED1 = 0,
     LED_LED2 = 1
 }LED_INDEX;
 
-typedef enum{
-    LED_BLINK_FREQ_1HZ = 1,
-    LED_BLINK_FREQ_2HZ = 2,
-    LED_BLINK_FREQ_4HZ = 4,
-    LED_BLINK_FREQ_8HZ = 8
-}LED_BLINK_FREQ;
-
 void LED_Init();
+void LED_BlinkSet(float freq, float duty);
 void LED_TurnOn(LED_INDEX i);
 void LED_TurnOff(LED_INDEX i);
 void LED_Reversal(LED_INDEX i);
-void LED_Blink_L1(LED_BLINK_FREQ freq, int seconds);
-void LED_Blink_L2(LED_BLINK_FREQ freq, int seconds);
 
 #endif
