c4312007209b17a9294f0daa29853339cade3c2b
github150620 <13508348543@139.com>
2019-02-25 19:46:59 +0800

    Update gsm.c

--- app/src/gsm.c
+++ app/src/gsm.c
@@ -8,15 +8,16 @@
 HANDLE gsm_lock = NULL;
 
 Network_Location_t gsm_wbuf[10];
-uint8_t gsm_wbuf_len;
+uint8_t gsm_wbuf_len = 0;
 Network_Location_t gsm_rbuf[10];
-uint8_t gsm_rbuf_len;
+uint8_t gsm_rbuf_len = 0;
 
 void GSM_Init() {
     gsm_lock = OS_CreateMutex();
 }
 
-void GSM_Update(Network_Location_t* location, uint8_t number) {
+void GSM_Update(uint8_t* data, uint8_t number) {
+    Network_Location_t* location = (Network_Location_t*)data;
     if (gsm_lock != NULL) {
         OS_LockMutex(gsm_lock);
         gsm_wbuf_len = number;
