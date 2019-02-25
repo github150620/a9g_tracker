57ff0f6227915ebeeb0bae39a35e5a9cad96cfeb
github150620 <13508348543@139.com>
2019-02-25 19:47:30 +0800

    Update gsm.h

--- app/include/gsm.h
+++ app/include/gsm.h
@@ -4,7 +4,7 @@
 #include "api_network.h"
 
 void GSM_Init();
-void GSM_Update(Network_Location_t* location, uint8_t number);
+void GSM_Update(uint8_t* data, uint8_t number);
 void GSM_GetLocation(Network_Location_t** location, uint8_t* len);
 
 #endif
